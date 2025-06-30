c3e5ce63e3fcde211e03057a0def33534cd34d1a
Souhailtounfi <souhailo2638@gmail.com>
2025-06-30 20:21:24 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+const basePath = process.env.NODE_ENV === 'production' ? '/Tazedates/' : '/';
+
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? basePath : '/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
